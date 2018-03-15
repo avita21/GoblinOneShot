2361367c55413fc33e4dad42cd784d2b286c6f25
Avita <avita.sharma@tufts.edu>
2018-03-14 22:34:27 -0400

    editing gitignore

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './style.css';
-
+import Phish from './react_phish';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 
 class App extends Component {
   render() {
     return (
+      <BrowserRouter>
+      <div>
+      <Route exact={true} path='/' render={() => (
       <div className="App">
+        <Phish />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
@@ -16,6 +21,9 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
+      )}/>
+      </div>
+      </BrowserRouter>
     );
   }
 }
