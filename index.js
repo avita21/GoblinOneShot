76b303a2ce9d6c521e534c69a177a64a17bab764
Avita <avita.sharma@tufts.edu>
2018-06-25 02:20:19 -0400

    finishing phish

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ var app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('port', (process.env.PORT || 5000));
+app.use(express.static('public'))
+
+app.post('/phish', (req, res) => {
+	res.send('You sent the  code: ' + req.body.Code + " !");
+});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
---
+++ public/phish.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	// Instantiate EasyZoom instances
+	$("#sheet").elevateZoom({
+		zoomType : "inner",
+        cursor : "crosshair"
+	});
+	console.log("elevate");
+});
